6f983d8509c7fd25338bb116804604e1f3084aa9
mike <iwarrior.xr@gmail.com>
2019-04-02 19:44:26 +0800

    first commit

--- src/main/scala/testPackage1/testS1.scala
+++ src/main/scala/testPackage1/testS1.scala
@@ -1,1 +1,19 @@
-package testPackage1import org.apache.flink.streaming.api.scala._class testS1 {  val env = StreamExecutionEnvironment.getExecutionEnvironment  val socketStream = env.socketTextStream("localhost",9000)  val wordsStream = socketStream.flatMap(value => value.split("\\s+")).map(value => (value,1))  val keyValuePair = wordsStream.keyBy(0)  val countPair = keyValuePair.sum(1)  countPair.print()  env.execute()}
+package testPackage1
+
+import org.apache.flink.streaming.api.scala._
+
+class testS1 {
+  val env = StreamExecutionEnvironment.getExecutionEnvironment
+  val socketStream = env.socketTextStream("localhost",9000)
+  val wordsStream = socketStream.flatMap(value => value.split("\\s+")).map(value => (value,1))
+
+  val wordsStreamABC = wordsStream.map(value => (value._1+"ABC", value._2))
+  val combinedStream = wordsStream.union(wordsStreamABC)
+
+  val keyValuePair = combinedStream.keyBy(0)
+  val countPair = keyValuePair.sum(1)
+
+  countPair.print()
+  env.execute()
+
+}

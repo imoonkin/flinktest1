16361659c4761b4cecce9f8f6477f5c071d9e082
mike <iwarrior.xr@gmail.com>
2019-04-18 00:57:22 +0800

    first commit

--- build.sbt
+++ build.sbt
@@ -6,4 +6,6 @@ scalaVersion := "2.12.8"
 
 libraryDependencies ++= Seq("org.apache.flink" %% "flink-scala" % "1.7.2",
   "org.apache.flink" %% "flink-clients" % "1.7.2",
-  "org.apache.flink" %% "flink-streaming-scala" % "1.7.2")
+  "org.apache.flink" %% "flink-streaming-scala" % "1.7.2",
+  "org.apache.kafka" %% "kafka" % "2.2.0"
+)
--- src/main/scala/testPackage1/testS1.scala
+++ src/main/scala/testPackage1/testS1.scala
@@ -1,12 +1,25 @@
 package testPackage1
 
+import org.apache.flink.streaming.api.CheckpointingMode
 import org.apache.flink.streaming.api.scala._
+import java.util.Properties
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+
 object testS1 {
 
   def main(args: Array[String]) {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.enableCheckpointing(7000, CheckpointingMode.EXACTLY_ONCE)
+    val properties = new Properties();
+    properties.setProperty("bootstrap.servers", "localhost:9092");
+    properties.setProperty("group.id", "test");
 
-    val socketStream = env.socketTextStream("localhost",9000)
+    val socketStream = env.addSource(
+      new FlinkKafkaConsumer[String](
+        "topiczhuheyi", new SimpleStringSchema(), properties))
+    //val socketStream = env.socketTextStream("localhost",9000)
     val flatStream = socketStream.flatMap(value => value.split("\\s+"))
 
     val w1=flatStream.map(value => (value, 1)).startNewChain().keyBy(0).sum(1)
